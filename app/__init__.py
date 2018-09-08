eb68e3e169822a08dcffb0e268b25c1c9b19cf5a
ankur <ankurj630@gmail.com>
2018-09-08 11:50:09 +0530

    swagger.yaml renamed to route.yaml

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from app.constant import SERVER
 def create_app():
     app = connexion.FlaskApp(__name__, instance_relative_config=True, swagger_ui=False)
     app.app.config.from_object(app_config[SERVER])
-    app.add_api('swagger.yaml')
+    app.add_api('route.yaml')
     if SERVER == 'development':
         app.debug = True
     return app
