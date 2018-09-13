d07b4694f1174df74cbf2d654b2d68247edde9cc
Ankur Jain <ankurj630@gmail.com>
2018-09-13 22:33:41 +0530

    test basic code added

---
+++ tests/test_a.py
@@ -0,0 +1,33 @@
+import os
+import unittest
+from app import create_app
+from api.models import db
+
+app = create_app()
+flask_app = app.app
+client = flask_app.test_client()
+
+
+class MyTestClass(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        with flask_app.app_context():
+            db.init_app(flask_app)
+            db.create_all()
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove('/tmp/test.db')
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_equal_numbers(self):
+        self.assertEqual(2, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
